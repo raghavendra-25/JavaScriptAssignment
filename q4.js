31429baa3a4f6ebaf764ea78647d764587e1ccaf
Raghavendra Reddy <82591835+raghavendra-25@users.noreply.github.com>
2021-04-22 09:21:58 +0530

    Update q4.js

--- q4.js
+++ q4.js
@@ -1,37 +1,37 @@
-let sa = function SortArray(inputArray){
-    this.originalArray = inputArray;
-    this.getSortedArray = function(inArray){
-      return arraySort(inArray);
-    }
-    let arraySort = function(arr){
-      arr.sort(function(a,b){return a-b;});
-      return arr;
-    }
+function SortArray(inputArray){
+  this.originalArray = inputArray;
+  this.getSortedArray = function(){
+    return arraySort(this.originalArray);
   }
-  
-  let inArray = [12,34,-5,3,24];
-  console.log(inArray);
-  let sarr = new sa(inArray);
-  console.log(sarr);
-  let newSorted = sarr.getSortedArray(inArray);
-  console.log(newSorted);
-  
-let soa = function SortObjectArray(originalArray,key){
-    this.key = key;
-    sa.call(this,originalArray);
-    this.getSortedArray = function(inArray,key){
-      return arraySort(inArray,key);
-    }
-    let arraySort = function(arr,key){
-      arr.sort(function(a,b){return a[key]-b[key];});
-      return arr;
-    }
+  let arraySort = function(arr){
+    arr.sort(function(a,b){return a-b;});
+    return arr;
   }
-  
-soa.prototype = Object.create(sa.prototype);
+}
+
+let inArray = [12,34,-5,3,24];
+console.log(inArray);
+let sarr = new SortArray(inArray);
+console.log(sarr);
+let newSorted = sarr.getSortedArray();
+console.log(newSorted);
+
+function SortObjectArray(originalArray,key){
+  this.key = key;
+  SortArray.call(this,originalArray);
+  this.getSortedArray = function(){
+    return arraySort(this.originalArray,this.key);
+  }
+  let arraySort = function(arr,key){
+    arr.sort(function(a,b){return a[key]-b[key];});
+    return arr;
+  }
+}
+
+SortObjectArray.prototype = new SortArray();
 
 let inArrayObjects = [{age:24},{age:34},{age:3},{age:22},{age:11}];
-let soarr = new soa(inArrayObjects,'age');
+let soarr = new SortObjectArray(inArrayObjects,'age');
 console.log(soarr);
-let newObjSorted = soarr.getSortedArray(inArrayObjects,'age');
+let newObjSorted = soarr.getSortedArray();
 console.log(newObjSorted);
